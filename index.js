3a0b17d0bad67aa752df3793adfad336e8eed419
ankush-kulashri <ankush.kulashri@gmail.com>
2022-06-12 12:25:28 +0530

    ok

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-function clock()
+function currentTime()
 {
-    console.log((new Date).getHours());
+    
+    time = new Date();
+    let sechands = time.getSeconds();
+    let minhands = time.getMinutes();
+    let hrhands = time.getHours();
+
+    var digi = document.getElementById('digi');
+    digi.innerHTML = hrhands+":"+minhands+":"+sechands;
+
+    var obj= document.getElementById('seconds');
+    sechands = 6*sechands;
+    obj.style.transform = 'rotate('+sechands+'deg)';
+
+    var minobj = document.getElementById('minutes');
+    minhands = 6*minhands;
+    minobj.style.transform = 'rotate('+minhands+'deg)';
+
+    var hrobj = document.getElementById('hour');
+    hrhands = 30*hrhands+(minhands/12);
+    hrobj.style.transform = 'rotate('+hrhands+'deg)';
+
+    
+
 }
 
-clock();
+setInterval(currentTime,1000);
